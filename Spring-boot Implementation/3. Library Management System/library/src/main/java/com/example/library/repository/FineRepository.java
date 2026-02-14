a245c38aa227c9ba963ac57c4ae8880be0be9276
MUTHUKUMARAN.N <muthuts2005@gmail.com>
2026-02-15 00:37:58 +0530

    Delete Spring-boot Implementation/3. Library Management System/library directory

--- Spring-boot Implementation/3. Library Management System/library/src/main/java/com/example/library/repository/FineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.library.repository;
-
-import com.example.library.model.Fine;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FineRepository extends JpaRepository<Fine, String> {
-}
