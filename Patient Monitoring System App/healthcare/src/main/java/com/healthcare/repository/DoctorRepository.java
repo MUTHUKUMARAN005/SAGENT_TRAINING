4052814734b5f3dc65dc24e421374ae549dab9c1
MUTHUKUMARAN.N <muthuts2005@gmail.com>
2026-03-02 22:56:39 +0530

    Delete Patient Monitoring System App directory

--- Patient Monitoring System App/healthcare/src/main/java/com/healthcare/repository/DoctorRepository.java
+++
@@ -1,8 +0,0 @@
-// File: src/main/java/com/healthcare/repository/DoctorRepository.java
-package com.healthcare.repository;
-
-import com.healthcare.model.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
-}
--- Patient Monitoring System App/healthcare/src/main/java/com/healthcare/repository/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-// File: src/main/java/com/healthcare/repository/PatientRepository.java
-package com.healthcare.repository;
-
-import com.healthcare.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
-}
