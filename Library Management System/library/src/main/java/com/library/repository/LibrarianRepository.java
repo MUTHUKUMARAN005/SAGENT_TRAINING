d2de312d23710015a01db11b7b11ad95881bb426
MUTHUKUMARAN.N <muthuts2005@gmail.com>
2026-03-02 22:56:05 +0530

    Delete Library Management System directory

--- Library Management System/library/src/main/java/com/library/repository/LibrarianRepository.java
+++
@@ -1,9 +0,0 @@
-// repository/LibrarianRepository.java
-package com.library.repository;
-
-import com.library.model.Librarian;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibrarianRepository extends JpaRepository<Librarian, String> {}
--- Library Management System/library/src/main/java/com/library/repository/LibraryRepository.java
+++
@@ -1,9 +0,0 @@
-// repository/LibraryRepository.java
-package com.library.repository;
-
-import com.library.model.Library;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibraryRepository extends JpaRepository<Library, String> {}
